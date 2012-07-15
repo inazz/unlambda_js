ba8110141aca1083941cb2f99d2f6f1d08cb95e8
inazz <inazz.git@gmail.com>
2012-07-15 13:25:36 +0900

    Add unlambda.Variable.gjstestEquals. it will be used to test parser.

---
+++ sources/tests/unlambda/variable_test.js
@@ -0,0 +1,34 @@
+
+var tests = tests || {};
+tests.unlambda = tests.unlambda || {};
+tests.unlambda.VariableTest = function() {};
+registerTestSuite(tests.unlambda.VariableTest);
+
+tests.unlambda.VariableTest.prototype.GjstestEquals = function() {
+  var variableI = new unlambda.Variable(unlambda.OP.I, null, null);
+  var variableI2 = new unlambda.Variable(unlambda.OP.I, null, null);
+  var variablePrintA = new unlambda.Variable(unlambda.OP.PRINT, 'a', null);
+  var variablePrintA2 = new unlambda.Variable(unlambda.OP.PRINT, 'a', null);
+  var variableApply1 = new unlambda.Variable(
+    unlambda.OP.APPLY, variablePrintA, variableI);
+  var variableApply2 = new unlambda.Variable(
+    unlambda.OP.APPLY, variablePrintA2, variableI2);
+  var variableApply3 = new unlambda.Variable(
+    unlambda.OP.APPLY, variableI, variableI2);
+
+  expectFalse(variableI.gjstestEquals(null));
+  expectFalse(variableI.gjstestEquals('i'));
+
+  expectTrue(variableI.gjstestEquals(variableI));
+  expectTrue(variableI.gjstestEquals(variableI2));
+  expectFalse(variableI.gjstestEquals(variablePrintA));
+  expectFalse(variableI.gjstestEquals(variableApply3));
+
+  expectFalse(variablePrintA.gjstestEquals(variableI));
+  expectTrue(variablePrintA.gjstestEquals(variablePrintA2));
+  expectFalse(variablePrintA.gjstestEquals(variableApply1));
+
+  expectTrue(variableApply1.gjstestEquals(variableApply1));
+  expectTrue(variableApply1.gjstestEquals(variableApply2));
+  expectFalse(variableApply1.gjstestEquals(variableApply3));
+};
--- sources/unlambda/variable.js
+++ sources/unlambda/variable.js
@@ -10,3 +10,30 @@ unlambda.Variable = function(op, v1, v2) {
   this.v1 = v1;
   this.v2 = v2;
 };
+
+unlambda.Variable.prototype.gjstestEquals = function(rhs) {
+  if (rhs === null || !(rhs instanceof Object) ||
+      this.constructor != rhs.constructor)
+    return false;
+  if (this.op != rhs.op)
+    return false;
+  var v1res = false
+  if (this.v1 === null) {
+    v1res = (rhs.v1 === null);
+  } else if (this.v1['gjstestEquals']) {
+    v1res = this.v1.gjstestEquals(rhs.v1);
+  } else {
+    v1res = (this.v1 == rhs.v1);
+  }
+  if (!v1res)
+    return false;
+  var v2res = false
+  if (this.v2 === null) {
+    v2res = (rhs.v2 === null);
+  } else if (this.v2['gjstestEquals']) {
+    v2res = this.v2.gjstestEquals(rhs.v2);
+  } else {
+    v2res = (this.v2 == rhs.v2);
+  }
+  return v2res;
+};
