09c10fcd68b216d2a4e80f47e71014d86b80b24e
inazz <inazz.git@gmail.com>
2012-07-15 09:45:14 +0900

    Rename Code to Variable.

--- sources/unlambda/variable.js
+++ sources/unlambda/variable.js
@@ -5,7 +5,7 @@ var unlambda = unlambda || {};
 // op -- unlambda.OP
 // v1 -- null or unlambda.Code
 // v2 -- null or unlambda.Code
-unlambda.Code = function(op, v1, v2) {
+unlambda.Variable = function(op, v1, v2) {
   this.op = op;
   this.v1 = v1;
   this.v2 = v2;
