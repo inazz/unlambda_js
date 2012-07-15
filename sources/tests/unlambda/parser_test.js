a026ff1867efc8bf29711d0ef5a45de3c09e1df1
inazz <inazz.git@gmail.com>
2012-07-15 14:59:27 +0900

    unlambda code parser.

--- sources/tests/unlambda/parser_test.js
+++ sources/tests/unlambda/parser_test.js
@@ -3,6 +3,115 @@ var tests = tests || {};
 tests.unlambda = tests.unlambda || {};
 tests.unlambda.parser = tests.unlambda.parser || {};
 
+tests.unlambda.parser.ParseTest = function() {
+  this.checkParseSuccess = function(code, expectedVariable) {
+    var res = unlambda.parser.parse(code);
+    expectTrue(res.success);
+    expectThat(res.error, isNull);
+    expectThat(res.variable, equals(expectedVariable));
+  };
+};
+registerTestSuite(tests.unlambda.parser.ParseTest);
+tests.unlambda.parser.ParseTest.prototype.Primitives = function() {
+  this.checkParseSuccess(
+    's', new unlambda.Variable(unlambda.OP.S, null, null));
+  this.checkParseSuccess(
+    'k', new unlambda.Variable(unlambda.OP.K, null, null));
+  this.checkParseSuccess(
+    'i', new unlambda.Variable(unlambda.OP.I, null, null));
+  this.checkParseSuccess(
+    'v', new unlambda.Variable(unlambda.OP.V, null, null));
+  this.checkParseSuccess(
+    'c', new unlambda.Variable(unlambda.OP.C, null, null));
+  this.checkParseSuccess(
+    'd', new unlambda.Variable(unlambda.OP.D, null, null));
+  this.checkParseSuccess(
+    '.a', new unlambda.Variable(unlambda.OP.PRINT, 'a', null));
+  this.checkParseSuccess(
+    'e', new unlambda.Variable(unlambda.OP.E, null, null));
+  this.checkParseSuccess(
+    '@', new unlambda.Variable(unlambda.OP.READ, null, null));
+  this.checkParseSuccess(
+    '?a', new unlambda.Variable(unlambda.OP.COMPARE, 'a', null));
+  this.checkParseSuccess(
+    '|', new unlambda.Variable(unlambda.OP.REPRINT, null, null));
+  this.checkParseSuccess(
+    'r', new unlambda.Variable(unlambda.OP.PRINT, "\n", null));
+};
+tests.unlambda.parser.ParseTest.prototype.TwoCharCornerCases = function() {
+  this.checkParseSuccess(
+    ".#", new unlambda.Variable(unlambda.OP.PRINT, '#', null));
+  this.checkParseSuccess(
+    ". ", new unlambda.Variable(unlambda.OP.PRINT, ' ', null));
+  this.checkParseSuccess(
+    ".\n", new unlambda.Variable(unlambda.OP.PRINT, "\n", null));
+  this.checkParseSuccess(
+    "?#", new unlambda.Variable(unlambda.OP.COMPARE, '#', null));
+  this.checkParseSuccess(
+    "? ", new unlambda.Variable(unlambda.OP.COMPARE, ' ', null));
+  this.checkParseSuccess(
+    "?\n", new unlambda.Variable(unlambda.OP.COMPARE, "\n", null));
+};
+tests.unlambda.parser.ParseTest.prototype.Apply = function() {
+  this.checkParseSuccess(
+    "`i`vk",
+    new unlambda.Variable(
+      unlambda.OP.APPLY,
+      new unlambda.Variable(unlambda.OP.I, null, null),
+      new unlambda.Variable(
+        unlambda.OP.APPLY,
+        new unlambda.Variable(unlambda.OP.V, null, null),
+        new unlambda.Variable(unlambda.OP.K, null, null))));
+};
+tests.unlambda.parser.ParseTest.prototype.CommendsAreIgnored = function() {
+  this.checkParseSuccess(
+    "  `i #test\nv\n",
+    new unlambda.Variable(
+      unlambda.OP.APPLY,
+      new unlambda.Variable(unlambda.OP.I, null, null),
+      new unlambda.Variable(unlambda.OP.V, null, null)));
+};
+tests.unlambda.parser.ParseTest.prototype.ExtraCharacter = function() {
+  var res = unlambda.parser.parse('`iiv');
+  expectFalse(res.success);
+  expectEq(unlambda.parser.ERROR.EXTRA_CHARACTER, res.error);
+  expectEq(3, res.error_pos);
+};
+tests.unlambda.parser.ParseTest.prototype.UnexpectedCharacter = function() {
+  var res = unlambda.parser.parse('``ihi');
+  expectFalse(res.success);
+  expectEq(unlambda.parser.ERROR.UNEXPECTED_CHARACTER, res.error);
+  expectEq(3, res.error_pos);
+};
+tests.unlambda.parser.ParseTest.prototype.UnexpectedEOF = function() {
+  var res = unlambda.parser.parse('');
+  expectFalse(res.success);
+  expectEq(unlambda.parser.ERROR.UNEXPECTED_EOF, res.error);
+  expectEq(0, res.error_pos);
+
+  res = unlambda.parser.parse('`');
+  expectFalse(res.success);
+  expectEq(unlambda.parser.ERROR.UNEXPECTED_EOF, res.error);
+  expectEq(1, res.error_pos);
+
+  res = unlambda.parser.parse('`i');
+  expectFalse(res.success);
+  expectEq(unlambda.parser.ERROR.UNEXPECTED_EOF, res.error);
+  expectEq(2, res.error_pos);
+
+  res = unlambda.parser.parse('`i.');
+  expectFalse(res.success);
+  expectEq(unlambda.parser.ERROR.UNEXPECTED_EOF, res.error);
+  expectEq(2, res.error_pos);
+
+  res = unlambda.parser.parse('`i?');
+  expectFalse(res.success);
+  expectEq(unlambda.parser.ERROR.UNEXPECTED_EOF, res.error);
+  expectEq(2, res.error_pos);
+};
+
+
+
 
 tests.unlambda.parser.FindEndOfCommentTest = function() {};
 registerTestSuite(tests.unlambda.parser.FindEndOfCommentTest);
--- sources/unlambda/parser.js
+++ sources/unlambda/parser.js
@@ -3,7 +3,11 @@
 var unlambda = unlambda || {};
 unlambda.parser = unlambda.parser || {};
 
-unlambda.parser.Result = function() {
+unlambda.parser.ParseResult = function() {
+  this.sucess = false;
+  this.variable = null; // unlambda.Variable
+  this.error = null; // unlambda.parser.ERROR
+  this.error_pos = -1;
 };
 
 unlambda.parser.ERROR = {
@@ -12,12 +16,72 @@ unlambda.parser.ERROR = {
   EXTRA_CHARACTER: 3,
 };
 
+unlambda.parser.ParseContext = function(code) {
+  this.code = code; // string
+  this.cur = 0;
+  this.end = code.length;
+  this.variable = null; // unlambda.Variable
+  this.error = null; // unlambda.parser.ERROR
+};
+
 
 // code -- string
 // rturn unlambda.parser.Result
 unlambda.parser.parse = function(code) {
-  var len = code.length;
-  // TODO(inazz): implement here.
+  var ctx = new unlambda.parser.ParseContext(code);
+  this.parse_(ctx);
+  if (ctx.error === null) {
+    ctx.cur = this.findEndOfComment(ctx.code, ctx.cur, ctx.end);
+    if (ctx.cur != ctx.end) {
+      ctx.error = unlambda.parser.ERROR.EXTRA_CHARACTER;
+    }
+  }
+  var res = new unlambda.parser.ParseResult();
+  res.success = (ctx.error === null);
+  res.variable = ctx.variable;
+  if (!res.success) {
+    res.error = ctx.error;
+    res.error_pos = ctx.cur;
+  }
+  return res;
+};
+
+// ctx -- unlambda.parser.ParseContext.
+unlambda.parser.parse_ = function(ctx) {
+  ctx.cur = this.findEndOfComment(ctx.code, ctx.cur, ctx.end);
+  if (ctx.cur == ctx.end) {
+    ctx.error = unlambda.parser.ERROR.UNEXPECTED_EOF;
+    return;
+  }
+  var c = ctx.code.charAt(ctx.cur);
+  if (c == "`") {
+    ctx.cur++;
+    this.parse_(ctx);
+    if (ctx.error !== null)
+      return;
+    var v1 = ctx.variable;
+    ctx.variable = null;
+    this.parse_(ctx);
+    if (ctx.error !== null)
+      return;
+    ctx.variable = new unlambda.Variable(c, v1, ctx.variable);
+  } else if ("skivcde@|".indexOf(c) != -1) {
+    ctx.variable = new unlambda.Variable(c, null, null);
+    ctx.cur++;
+  } else if (".?".indexOf(c) != -1) {
+    if (ctx.cur + 1 == ctx.end) {
+      ctx.error = unlambda.parser.ERROR.UNEXPECTED_EOF;
+    } else {
+      ctx.variable =
+        new unlambda.Variable(c, ctx.code.charAt(ctx.cur + 1), null);
+      ctx.cur += 2;
+    }
+  } else if (c == "r") {
+    ctx.variable = new unlambda.Variable(".", "\n", null);
+    ctx.cur++;
+  } else {
+    ctx.error = unlambda.parser.ERROR.UNEXPECTED_CHARACTER;
+  }
 };
 
 
