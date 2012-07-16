cdfcfa391f477725378f5e28e9c4b3c9b40cc140
inazz <inazz.git@gmail.com>
2012-07-16 11:53:55 +0900

    Add source_files.rb to specify dependency between js files. Change run_tests.rb to use source_files.rb to find correct jsfiles order. Also change run_tests.rb to limit executing tests if argument is given. "./run_tests.rb" executes all tests. "./run_tests.rb op" executes all tests on files with their filename include 'op'.

--- scripts/run_tests.rb
+++ scripts/run_tests.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/ruby
 
 require 'find'
+require './source_files.rb'
 
 class TestRunner
 
-  def main
+  def main(args)
     moveToRepositoryRoot()
-    runTests()
+    runTests(args)
   end
 
   def moveToRepositoryRoot()
@@ -19,19 +20,14 @@ class TestRunner
       "Please execute this script from directory in the repository.\n"
   end
 
-  def runTests()
-    sources = getJsFiles('sources/unlambda')
-    tests = getJsFiles('sources/tests')
-    system('gjstest', '--js_files=' + (sources + tests).join(','))
-  end
-
-  def getJsFiles(dir)
-    files = []
-    Find.find(dir) {|f|
-      files.push(f) if f =~/\.js$/
-    }
-    return files
+  def runTests(args)
+    files = SourceFiles.new.getFileList()
+    if (args.length != 0)
+      files.reject!{|f| f =~/\_test\.js$/ && !f.match(args[0])}
+    end
+    files.map!{|f| 'sources/' + f}
+    system('gjstest', '--js_files=' + files.join(','))
   end
 end
 
-TestRunner.new.main()
+TestRunner.new.main(ARGV)
---
+++ scripts/source_files.rb
@@ -0,0 +1,39 @@
+
+class SourceFiles
+
+  def getDependencyHash()
+    return {
+      'unlambda/op.js' => [],
+      'unlambda/variable.js' => ['unlambda/op.js'],
+      'unlambda/parser.js' => ['unlambda/op.js', 'unlambda/variable.js'],
+      'tests/unlambda/op_test.js' => ['unlambda/op.js'],
+      'tests/unlambda/parser_test.js' => ['unlambda/parser.js'],
+      'tests/unlambda/variable_test.js' => ['unlambda/variable.js'],
+    }
+  end
+  def getFileList()
+    nameToScore = {}
+    depsHash = getDependencyHash()
+    # scoring
+    depsHash.keys.each{|file|
+      scoreFileDfs(file, nameToScore, depsHash)
+    }
+    # sanitize
+    depsHash.each{|file, list|
+      list.each{|dep|
+        if (nameToScore[file] <= nameToScore[dep])
+          raise 'dependency broken. detected a loop including "' + file + '".'
+        end
+      }
+    }
+    # sort
+    return depsHash.keys.sort_by{|f| nameToScore[f]}
+  end
+  def scoreFileDfs(file, nameToScore, depsHash)
+    return nameToScore[file] if nameToScore.has_key?(file)
+    raise 'missing dependency for "' + file + '".' if !depsHash.has_key?(file)
+    deps = depsHash[file]
+    score = (deps.map{|d| scoreFileDfs(d, nameToScore, depsHash)+1} + [0]).max
+    return nameToScore[file] = score;
+  end
+end
