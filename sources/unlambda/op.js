401f8daefbe3e4a6829f49fba6818a1abb504bbd
inazz <inazz.git@gmail.com>
2012-07-16 13:39:09 +0900

    Add comment about what is expected v1 and v2 for each OP.

--- sources/unlambda/op.js
+++ sources/unlambda/op.js
@@ -1,22 +1,22 @@
 
 var unlambda = unlambda || {};
 
-unlambda.OP = {
-  APPLY:   '`',
+unlambda.OP = {  // comments are expected Variable.v1 and v2.
+  APPLY:   '`',  // not evluated variable, not evaluated variable
   S:       's',
-  S1:      's1',
-  S2:      's2',
+  S1:      's1', // variable
+  S2:      's2', // variable, variable
   K:       'k',
-  K1:      'k1',
+  K1:      'k1', // variable
   I:       'i',
   V:       'v',
   C:       'c',
-  C1:      'c1',
+  C1:      'c1', // root_variable, current_character
   D:       'd',
-  D1:      'd1',
-  PRINT:   '.',
+  D1:      'd1', // not evaluated variable
+  PRINT:   '.',  // string of length 1
   E:       'e',
   READ:    '@',
-  COMPARE: '?',
+  COMPARE: '?',  // string of length 1
   REPRINT: '|',
 };
