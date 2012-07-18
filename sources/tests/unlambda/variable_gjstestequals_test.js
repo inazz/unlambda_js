442888c6c31b2e35e045189b13b6d10a0abad88a
inazz <inazz.git@gmail.com>
2012-07-18 10:11:29 +0900

    Change unlambda.Variable.gjstestequals to support looping variable.

--- sources/tests/unlambda/variable_gjstestequals_test.js
+++ sources/tests/unlambda/variable_gjstestequals_test.js
@@ -30,3 +30,28 @@ UnlambdaVariableGjstestequalsTest.prototype.GjstestEquals = function() {
   expectTrue(variableApply1.gjstestEquals(variableApply2));
   expectFalse(variableApply1.gjstestEquals(variableApply3));
 };
+
+UnlambdaVariableGjstestequalsTest.prototype.GjstestEqualsLoop = function() {
+  var variableC1 = new unlambda.Variable(unlambda.OP.C1, null, 'x');
+  var variableC12 = new unlambda.Variable(unlambda.OP.C1, null, 'x');
+  var variableC13 = new unlambda.Variable(unlambda.OP.C1, null, 'x');
+  var variableI = new unlambda.Variable(unlambda.OP.I, null, null);
+  var variableI2 = new unlambda.Variable(unlambda.OP.I, null, null);
+  var variableV = new unlambda.Variable(unlambda.OP.V, null, null);
+  var variableApply1 = new unlambda.Variable(
+    unlambda.OP.APPLY, variableC1, variableI);
+  variableC1.v1 = variableApply1;
+  var variableApply2 = new unlambda.Variable(
+    unlambda.OP.APPLY, variableC12, variableI2);
+  variableC12.v1 = variableApply2;
+
+  expectTrue(variableApply1.gjstestEquals(variableApply2));
+
+  variableC12.v2 = 'y';
+  expectFalse(variableApply1.gjstestEquals(variableApply2));
+  variableC12.v2 = 'x';
+
+  variableApply2.v2 = variableV;
+  expectFalse(variableApply1.gjstestEquals(variableApply2));
+  variableApply2.v2 = variableI2;
+};
--- sources/unlambda/variable_gjstestequals.js
+++ sources/unlambda/variable_gjstestequals.js
@@ -3,26 +3,36 @@
 var unlambda = unlambda || {};
 
 unlambda.Variable.prototype.gjstestEquals = function(rhs) {
+  var memo = {};
+  return this.gjstestEquals_(memo, rhs);
+};
+unlambda.Variable.prototype.gjstestEquals_ = function(memo, rhs) {
+  if (this === rhs)
+    return true;
   if (rhs === null || !(rhs instanceof Object) ||
       this.constructor != rhs.constructor)
     return false;
+  if (!(this.id in memo)) {
+    memo[this.id] = {};
+  }
+  if (rhs.id in memo[this.id]) {
+    return true;
+  } else {
+    memo[this.id][rhs.id] = true;
+  }
   if (this.op != rhs.op)
     return false;
-  var v1res = false
-  if (this.v1 === null) {
-    v1res = (rhs.v1 === null);
-  } else if (this.v1['gjstestEquals']) {
-    v1res = this.v1.gjstestEquals(rhs.v1);
+  var v1res = false;
+  if (this.v1 instanceof Object && this.constructor == this.v1.constructor) {
+    v1res = this.v1.gjstestEquals_(memo, rhs.v1);
   } else {
     v1res = (this.v1 == rhs.v1);
   }
   if (!v1res)
     return false;
   var v2res = false
-  if (this.v2 === null) {
-    v2res = (rhs.v2 === null);
-  } else if (this.v2['gjstestEquals']) {
-    v2res = this.v2.gjstestEquals(rhs.v2);
+  if (this.v2 instanceof Object && this.constructor == this.v2.constructor) {
+    v2res = this.v2.gjstestEquals_(memo, rhs.v2);
   } else {
     v2res = (this.v2 == rhs.v2);
   }
