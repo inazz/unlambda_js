cb765f6a38848abc696176c822853904c1fcbcab
inazz <inazz.git@gmail.com>
2012-07-29 18:09:19 +0900

    naive implementation for OutputPanel.

--- sources/page/output_panel.js
+++ sources/page/output_panel.js
@@ -7,8 +7,25 @@ var page = page || {};
 page.OutputPanel = function(app, dom_helper) {
   this.app = app;
   this.dom_helper = dom_helper;
+  this.output_block = null;
+  this.last_text_node = null;
 };
 
 page.OutputPanel.prototype.init = function() {
-  // TODO.
+  this.output_block = this.dom_helper.get('outputblock');
+};
+
+page.OutputPanel.prototype.clear = function() {
+  this.dom_helper.removeChildren(this.output_block);
+  this.last_text_node = null;
+};
+
+page.OutputPanel.prototype.appendOutput = function(str) {
+  if (this.last_text_node == null) {
+    var span = this.dom_helper.createElement('span');
+    this.last_text_node = this.dom_helper.createTextNode();
+    this.dom_helper.appendChild(span, this.last_text_node);
+    this.dom_helper.appendChild(this.output_block, span);
+  }
+  this.dom_helper.appendData(this.last_text_node, str);
 };
--- sources/util/dom_helper.js
+++ sources/util/dom_helper.js
@@ -22,3 +22,31 @@ util.DomHelper.prototype.setEventListener = function(dom, type, obj, func) {
 util.DomHelper.prototype.getValue = function(dom) {
   return dom.value;
 };
+
+// String
+util.DomHelper.prototype.createElement = function(name) {
+  return this.doc.createElement(name);
+};
+
+util.DomHelper.prototype.createTextNode = function() {
+  var node = this.doc.createTextNode();
+  node.data = '';
+  return node;
+};
+
+// HtmlElement(TextNode), String
+util.DomHelper.prototype.appendData = function(textNode, str) {
+  textNode.appendData(str);
+};
+
+// HtmlElement, HtmlElement
+util.DomHelper.prototype.appendChild = function(parentNode, childNode) {
+  parentNode.appendChild(childNode);
+};
+
+// HtmlElement
+util.DomHelper.prototype.removeChildren = function(dom) {
+  while (dom.hasChildNodes()) {
+    dom.removeChild(dom.firstChild);
+  }
+};
