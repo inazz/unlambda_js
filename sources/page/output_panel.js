dd8359373c6559af74a095a191e0541c5df933b8
inazz <inazz.git@gmail.com>
2012-08-16 16:06:09 +0900

    Implement OutputPanel.appendInputEchoBack().

--- sources/page/output_panel.js
+++ sources/page/output_panel.js
@@ -9,6 +9,7 @@ page.OutputPanel = function(app, dom_helper) {
   this.dom_helper = dom_helper;
   this.output_block = null;
   this.last_text_node = null;
+  this.last_span_class_name = '';
 };
 
 page.OutputPanel.prototype.init = function() {
@@ -18,18 +19,26 @@ page.OutputPanel.prototype.init = function() {
 page.OutputPanel.prototype.clear = function() {
   this.dom_helper.removeChildren(this.output_block);
   this.last_text_node = null;
+  this.last_span_class_name = '';
 };
 
 page.OutputPanel.prototype.appendOutput = function(str) {
-  if (this.last_text_node == null) {
+  this.appendSpanTextWithClassName_('output', str);
+};
+
+page.OutputPanel.prototype.appendInputEchoBack = function(str) {
+  this.appendSpanTextWithClassName_('echo_back', str);
+};
+
+page.OutputPanel.prototype.appendSpanTextWithClassName_ = function(classname, str) {
+  if (this.last_span_class_name != classname) {
     var span = this.dom_helper.createElement('span');
+    this.dom_helper.addClass(span, classname);
     this.dom_helper.appendChild(this.output_block, span);
     this.last_text_node = this.dom_helper.createTextNode();
     this.dom_helper.appendChild(span, this.last_text_node);
+    this.last_span_class_name = classname;
   }
   this.dom_helper.appendData(this.last_text_node, str);
 };
 
-page.OutputPanel.prototype.appendInputEchoBack = function(str) {
-  // TODO.
-};
--- sources/tests/page/output_panel_test.js
+++ sources/tests/page/output_panel_test.js
@@ -19,14 +19,20 @@ PageOutputPanelTest.prototype.InitGrabsDom = function() {
 
 PageOutputPanelTest.prototype.ClearRemovesAll = function() {
   expectCall(this.dom_helper.removeChildren)(this.panel.output_block);
+  this.panel.last_text_node = {};
+  this.panel.last_span_class_name = 'hoge';
 
   this.panel.clear();
+  expectNe(null, this.panel.output_block);
+  expectEq(null, this.panel.last_text_node);
+  expectEq('', this.panel.last_span_class_name);
 };
 
 PageOutputPanelTest.prototype.AppendOutputCreatesSpanOnce = function() {
   var span = {}, text_node = {};
 
   expectCall(this.dom_helper.createElement)('span').willOnce(returnWith(span));
+  expectCall(this.dom_helper.addClass)(span, 'output');
   expectCall(this.dom_helper.createTextNode)().willOnce(returnWith(text_node));
   expectCall(this.dom_helper.appendChild)(this.panel.output_block, span);
   expectCall(this.dom_helper.appendChild)(span, text_node);
@@ -35,4 +41,51 @@ PageOutputPanelTest.prototype.AppendOutputCreatesSpanOnce = function() {
 
   expectCall(this.dom_helper.appendData)(text_node, 'fuga');
   this.panel.appendOutput('fuga');
+};
+
+PageOutputPanelTest.prototype.AppendInputEchoBackCreatesSpanOnce = function() {
+  var span = {}, text_node = {};
+
+  expectCall(this.dom_helper.createElement)('span').willOnce(returnWith(span));
+  expectCall(this.dom_helper.addClass)(span, 'echo_back');
+  expectCall(this.dom_helper.createTextNode)().willOnce(returnWith(text_node));
+  expectCall(this.dom_helper.appendChild)(this.panel.output_block, span);
+  expectCall(this.dom_helper.appendChild)(span, text_node);
+  expectCall(this.dom_helper.appendData)(text_node, 'hoge');
+  this.panel.appendInputEchoBack('hoge');
+
+  expectCall(this.dom_helper.appendData)(text_node, 'fuga');
+  this.panel.appendInputEchoBack('fuga');
+};
+
+PageOutputPanelTest.prototype.OutputCreatesNewSpanWhenClassNameChanged = function() {
+  var span1 = {}, span2 = {}, span3 = {};
+  var text_node1 = {}, text_node2 = {}, text_node3 = {};
+
+  expectCall(this.dom_helper.createElement)('span')
+    .willOnce(returnWith(span1))
+    .willOnce(returnWith(span2))
+    .willOnce(returnWith(span3));
+  expectCall(this.dom_helper.createTextNode)()
+    .willOnce(returnWith(text_node1))
+    .willOnce(returnWith(text_node2))
+    .willOnce(returnWith(text_node3));
+  expectCall(this.dom_helper.addClass)(span1, 'output');
+  expectCall(this.dom_helper.appendChild)(this.panel.output_block, span1);
+  expectCall(this.dom_helper.appendChild)(span1, text_node1);
+  expectCall(this.dom_helper.appendData)(text_node1, 'a');
+
+  expectCall(this.dom_helper.addClass)(span2, 'echo_back');
+  expectCall(this.dom_helper.appendChild)(this.panel.output_block, span2);
+  expectCall(this.dom_helper.appendChild)(span2, text_node2);
+  expectCall(this.dom_helper.appendData)(text_node2, 'b');
+
+  expectCall(this.dom_helper.addClass)(span3, 'output');
+  expectCall(this.dom_helper.appendChild)(this.panel.output_block, span3);
+  expectCall(this.dom_helper.appendChild)(span3, text_node3);
+  expectCall(this.dom_helper.appendData)(text_node3, 'c');
+
+  this.panel.appendOutput('a');
+  this.panel.appendInputEchoBack('b');
+  this.panel.appendOutput('c');
 };
