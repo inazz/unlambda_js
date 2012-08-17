f3a73c7793f38ea02b3985ab4d9341358f7ca4d3
inazz <inazz.git@gmail.com>
2012-08-17 17:24:33 +0900

    Add sample code selector.

--- sources/page/code_panel.js
+++ sources/page/code_panel.js
@@ -7,21 +7,51 @@ page.CodePanel = function(app, dom_helper) {
   this.app = app;
   this.dom_helper = dom_helper;
   this.code_area = null;
+  this.code_selector = null;
 };
+
+page.CodePanel.HELLO_WORLD_CODE = "`````````````.H.e.l.l.o.,. .w.o.r.l.d.!i";
+page.CodePanel.HELLO_WORLD_SHORT_CODE = "`.!`.d`.l`.r``.w`. `.,``.l`c`.H.e.oi";
 
 page.CodePanel.prototype.init = function() {
   this.code_area = this.dom_helper.get('code');
+  this.code_selector = this.dom_helper.get('code_selector');
+  this.dom_helper.addEventListener(
+    this.code_area, 'keydown', this, this.onCodeChange);
+  this.dom_helper.addEventListener(
+    this.code_area, 'change', this, this.onCodeChange);
+  this.dom_helper.addEventListener(
+    this.code_selector, 'change', this, this.onSelectorChange);
 };
 
 page.CodePanel.prototype.getCode = function() {
   return this.dom_helper.getValue(this.code_area);
 };
 
+page.CodePanel.prototype.onCodeChange = function(ev) {
+  if (this.dom_helper.getValue(this.code_selector) != "") {
+    this.dom_helper.setValue(this.code_selector, "");
+  }
+};
+
+page.CodePanel.prototype.onSelectorChange = function(ev) {
+  var sample_name = this.dom_helper.getValue(this.code_selector);
+  if (sample_name == "hello_world") {
+    this.dom_helper.setValue(
+      this.code_area, page.CodePanel.HELLO_WORLD_CODE);
+  } else if (sample_name == "hello_world_short") {
+    this.dom_helper.setValue(
+      this.code_area, page.CodePanel.HELLO_WORLD_SHORT_CODE);
+  }
+};
+
 page.CodePanel.prototype.updateView = function() {
   var ctx = this.app.getAppContext();
   if (ctx.run_state == unlambda_app.RUN_STATE.STOPPED) {
+    this.dom_helper.enable(this.code_selector, true);
     this.dom_helper.enable(this.code_area, true);
   } else {
+    this.dom_helper.enable(this.code_selector, false);
     this.dom_helper.enable(this.code_area, false);
   }
 };
--- sources/tests/page/code_panel_test.js
+++ sources/tests/page/code_panel_test.js
@@ -4,41 +4,84 @@ function PageCodePanelTest() {
   this.dom_helper = createMockInstance(util.DomHelper);
   this.panel = new page.CodePanel(this.app, this.dom_helper);
   this.app.code_panel = this.panel;
+  this.panel.code_area = {};
+  this.panel.code_selector = {};
 }
 registerTestSuite(PageCodePanelTest);
 
-PageCodePanelTest.prototype.initGrabCodeArea = function() {
-  var code_area = {};
+PageCodePanelTest.prototype.initRegisterCallbacks = function() {
+  var code_area = {}, code_selector = {};
   expectCall(this.dom_helper.get)('code').willOnce(returnWith(code_area));
+  expectCall(this.dom_helper.get)('code_selector')
+    .willOnce(returnWith(code_selector));
+  expectCall(this.dom_helper.addEventListener)(
+    code_area, 'keydown', this.panel, this.panel.onCodeChange);
+  expectCall(this.dom_helper.addEventListener)(
+    code_area, 'change', this.panel, this.panel.onCodeChange);
+  expectCall(this.dom_helper.addEventListener)(
+    code_selector, 'change', this.panel, this.panel.onSelectorChange);
 
   this.panel.init();
   expectEq(code_area, this.panel.code_area);
+  expectEq(code_selector, this.panel.code_selector);
 };
 
 PageCodePanelTest.prototype.GetCode = function() {
-  this.panel.code_area = {};
   var code = "hoge";
   expectCall(this.dom_helper.getValue)(this.panel.code_area).willOnce(
     returnWith(code));
   expectEq(code, this.panel.getCode());
 };
 
+PageCodePanelTest.prototype.OnCodeChangeDoNothingWhenNotASample = function() {
+  expectCall(this.dom_helper.getValue)(this.panel.code_selector)
+    .willOnce(returnWith(""));
+  this.panel.onCodeChange();
+};
+
+PageCodePanelTest.prototype.OnCodeChangeMakeSelectorDefaut = function() {
+  expectCall(this.dom_helper.getValue)(this.panel.code_selector)
+    .willOnce(returnWith("hello_world"));
+  expectCall(this.dom_helper.setValue)(this.panel.code_selector, "");
+  this.panel.onCodeChange();
+};
+
+PageCodePanelTest.prototype.OnSelectorChangeDoNothingWhenDefault = function() {
+  expectCall(this.dom_helper.getValue)(this.panel.code_selector)
+    .willOnce(returnWith(""));
+  this.panel.onSelectorChange();
+};
+
+PageCodePanelTest.prototype.OnSelectorChangeHelloWorld = function() {
+  expectCall(this.dom_helper.getValue)(this.panel.code_selector)
+    .willOnce(returnWith("hello_world"));
+  expectCall(this.dom_helper.setValue)(
+    this.panel.code_area, page.CodePanel.HELLO_WORLD_CODE);
+  this.panel.onSelectorChange();
+};
+
+PageCodePanelTest.prototype.OnSelectorChangeHelloWorldShort = function() {
+  expectCall(this.dom_helper.getValue)(this.panel.code_selector)
+    .willOnce(returnWith("hello_world_short"));
+  expectCall(this.dom_helper.setValue)(
+    this.panel.code_area, page.CodePanel.HELLO_WORLD_SHORT_CODE);
+  this.panel.onSelectorChange();
+};
+
 PageCodePanelTest.prototype.UpdateViewEnableEditWhenStopped = function() {
   var ctx = this.app.getAppContext();
-  var code_area = {};
-  this.panel.code_area = code_area;
 
   ctx.run_state = unlambda_app.RUN_STATE.STOPPED;
-  expectCall(this.dom_helper.enable)(code_area, true);
+  expectCall(this.dom_helper.enable)(this.panel.code_area, true);
+  expectCall(this.dom_helper.enable)(this.panel.code_selector, true);
   this.panel.updateView();
 };
 
 PageCodePanelTest.prototype.UpdateViewDisableEditWhenNotStopped = function() {
   var ctx = this.app.getAppContext();
-  var code_area = {};
-  this.panel.code_area = code_area;
 
   ctx.run_state = unlambda_app.RUN_STATE.PAUSED;
-  expectCall(this.dom_helper.enable)(code_area, false);
+  expectCall(this.dom_helper.enable)(this.panel.code_area, false);
+  expectCall(this.dom_helper.enable)(this.panel.code_selector, false);
   this.panel.updateView();
 };
